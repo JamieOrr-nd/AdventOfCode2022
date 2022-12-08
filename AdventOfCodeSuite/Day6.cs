5f84c9d5c22fa4044ffd626d34e92c845316c659
Jamie Orr <Jamie.Orr@newday.co.uk>
2022-12-08 15:33:50 +0000

    solved Day6

--- AdventOfCodeSuite/Day6.cs
+++ AdventOfCodeSuite/Day6.cs
@@ -8,5 +8,32 @@ namespace AdventOfCodeSuite
 {
     internal class Day6
     {
+        internal int FindfirstBlockOfFour(string input)
+        {
+            int index = 2;
+            bool blockFound = false;
+
+            while (blockFound == false)
+            {
+                ++index;
+                blockFound = new string(input.Substring(index - 3, 4).ToCharArray().Distinct().ToArray()).Length == 4;
+            }
+
+            return index + 1;
+        }
+
+        internal int FindfirstBlockOfFourteen(string input)
+        {
+            int index = 2;
+            bool blockFound = false;
+
+            while (blockFound == false)
+            {
+                ++index;
+                blockFound = new string(input.Substring(index - 13, 14).ToCharArray().Distinct().ToArray()).Length == 14;
+            }
+
+            return index + 1;
+        }
     }
 }
--- AdventOfCodeSuite/Tests6To10.cs
+++ AdventOfCodeSuite/Tests6To10.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace AdventOfCodeSuite
 {
-    internal class Tests6To10
+    public class Tests6To10
     {
+        [Theory]
+        [InlineData("abcabcabcd", 10)]
+        public void Day6Puzzle1(string input, int result)
+        {
+            // Arrange
+            Day6 day6 = new Day6();
+
+            // Act 
+            int expected = result;
+            int actual = day6.FindfirstBlockOfFour(input);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("abcabcabcd", 10)]
+        public void Day7Puzzle1(string input, int result)
+        {
+            // Arrange
+            Day6 day6 = new Day6();
+
+            // Act 
+            int expected = result;
+            int actual = day6.FindfirstBlockOfFour(input);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
     }
 }
